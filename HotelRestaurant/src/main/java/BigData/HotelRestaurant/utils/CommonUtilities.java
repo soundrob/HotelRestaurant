9601d68f98b92f8f7d5d8319abee121ebe43bfd1
paltanrobert <paltanrobert@yahoo.com>
2018-04-16 17:27:11 +0300

    comit robi2

--- HotelRestaurant/src/main/java/BigData/HotelRestaurant/utils/CommonUtilities.java
+++ HotelRestaurant/src/main/java/BigData/HotelRestaurant/utils/CommonUtilities.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 
 public class CommonUtilities {
 	
-	//put your server url here dragos
+	//put your server url here dragos olaru
 	public static final String SERVER_URL ="SendPushNotification.php";
 	// Google project id
 	public static final String SENDER_ID = "restaurant-200915";
--- HotelRestaurant/src/main/java/BigData/HotelRestaurant/utils/ConnectionDetector.java
+++ HotelRestaurant/src/main/java/BigData/HotelRestaurant/utils/ConnectionDetector.java
@@ -30,7 +30,7 @@ public class ConnectionDetector {
 	          return false;
 	    }
 	
-	
+	//zuzuzuzuzuzu
 	
 
 }
